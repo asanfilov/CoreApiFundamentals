6387f01f0cc2d50474aefaadb855cc663ce70dda
asanfilov <25196996+asanfilov@users.noreply.github.com>
2021-05-01 21:18:10 -0300

    BYFA: Using GET for Collections

---
+++ src/Controllers/CampsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+
+using CoreCodeCamp.Data;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route( "api/[controller]" )]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository repository;
+
+        public CampsController(ICampRepository repo)
+        {
+            this.repository = repo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var camps = await this.repository.GetAllCampsAsync();
+                return Ok( camps );
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode( StatusCodes.Status500InternalServerError, "Database failure" );
+            }
+        }
+    }
+}
