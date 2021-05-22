74f9718fd62dc0fb8d43a82e8fd2c45303572a5e
asanfilov <25196996+asanfilov@users.noreply.github.com>
2021-05-22 15:21:00 -0300

    Format code

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -48,15 +48,13 @@ namespace CoreCodeCamp.Controllers
             try
             {
                 var camp = await repository.GetCampAsync( moniker );
-                if (camp == null)
-                {
-                    return NotFound();
-                }
+                if (camp == null) return NotFound();
+
                 return mapper.Map<CampModel>( camp );
             }
             catch (Exception ex)
             {
-                return this.StatusCode( StatusCodes.Status500InternalServerError, "Database failure" );
+                return StatusCode( StatusCodes.Status500InternalServerError, "Database failure" );
             }
         }
 
@@ -72,7 +70,7 @@ namespace CoreCodeCamp.Controllers
             }
             catch (Exception ex)
             {
-                return this.StatusCode( StatusCodes.Status500InternalServerError, "Database failure" );
+                return StatusCode( StatusCodes.Status500InternalServerError, "Database failure" );
             }
         }
 
@@ -81,28 +79,23 @@ namespace CoreCodeCamp.Controllers
             try
             {
                 var existing = await repository.GetCampAsync( campModel.Moniker );
-                if (existing != null)
-                {
-                    return BadRequest( "Moniker is in use" );
-                }
+                if (existing != null) return BadRequest( "Moniker is in use" );
 
                 string createdAt = linkGenerator.GetPathByAction( "GET", "camps",
                                                                 new { moniker = campModel.Moniker } );
-                if (string.IsNullOrWhiteSpace( createdAt ))
-                {
-                    return BadRequest( "Could not use current moniker" );
-                }
+                if (string.IsNullOrWhiteSpace( createdAt )) return BadRequest( "Could not use current moniker" );
 
                 var camp = mapper.Map<Camp>( campModel );
                 repository.Add( camp );
-                if (await repository.SaveChangesAsync())
-                {
-                    return Created( createdAt, mapper.Map<CampModel>( camp ) );
-                }
+                if (await repository.SaveChangesAsync()) return Created( createdAt, mapper.Map<CampModel>( camp ) );
             }
             catch (Exception ex)
             {
-                return this.StatusCode( StatusCodes.Status500InternalServerError, "Database failure" );
+                return StatusCode( StatusCodes.Status500InternalServerError, "Database failure" );
+            }
+
+            return BadRequest();
+        }
             }
             return BadRequest();
         }
