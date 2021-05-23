3042b7f9cfe19362f7ddb04448c6bca5fa7e90dd
asanfilov <25196996+asanfilov@users.noreply.github.com>
2021-05-23 16:48:15 -0300

    Finish Creating a Functional API. Delete the ValuesController

---
+++ src/Controllers/OperationsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route( "api/[controller]" )]
+    [ApiController]
+    public class OperationsController : ControllerBase
+    {
+        private readonly IConfiguration config;
+
+        public OperationsController(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        [HttpOptions( "reloadconfig" )]
+        public IActionResult ReloadConfig()
+        {
+            try
+            {
+                var root = (IConfigurationRoot)config;
+                root.Reload();
+                return Ok();
+            }
+            catch (System.Exception)
+            {
+                return StatusCode( StatusCodes.Status500InternalServerError );
+            }
+        }
+    }
+}
